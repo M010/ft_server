c753765a0e274fc54cae860b3f8f677cee5701b0
moh <you@example.com>
2020-10-27 20:55:18 +0300

    final

--- srcs/tog_index.sh
+++ srcs/tog_index.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cp /etc/nginx/sites-available/the_site /cnf/tmp
-mv /cnf/nginx_conf_2  /etc/nginx/sites-available/the_site/ 
+mv /cnf/nginx_conf_2  /etc/nginx/sites-available/the_site
 mv /cnf/tmp /cnf/nginx_conf_2
 nginx -s reload 
-
