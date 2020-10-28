f41191029dc007c39ce390923bf8d810f5971ef5
moh <you@example.com>
2020-10-28 11:36:14 +0300

    /dev

--- srcs/set_up.sh
+++ srcs/set_up.sh
@@ -2,8 +2,8 @@
 
 service mysql start
 mysql -u 	root --skip-password -e "CREATE DATABASE wordpress;"
-mysql -u 	root --skip-password -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" 2&> /dev/null
-mysql -u 	root --skip-password -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root');FLUSH PRIVILEGES;" 2&> /dev/null
+mysql -u 	root --skip-password -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" &2>/dev/null
+mysql -u 	root --skip-password -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root');FLUSH PRIVILEGES;" &2> /dev/null 
 
 service		nginx		start
 service 	php7.3-fpm	start
